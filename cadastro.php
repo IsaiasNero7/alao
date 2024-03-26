78884753e43ed8d83393d6c79e09089a18c2da4c
Kayki-Borges <is.isaias777@gmail.com>
2024-03-26 09:00:31 -0300

    v3
    Banco de dados

--- cadastro.php
+++ cadastro.php
@@ -13,14 +13,38 @@
         <div class="row">
 
             <div class="col">
+                <h1>Cadastre-se</h1>
+                    <form action="cad.php" method="post">
+                        <div class="mb-3">
+                            <label for="nome" class="form-label">Nome completo</label>
+                            <input type="text" class="form-control" name="nome">
 
+                            <label for="endereco" class="form-label">Endereço</label>
+                            <input type="text" class="form-control" name="endereco">
+
+                            <label for="telefone" class="form-label">Telefone</label>
+                            <input type="text" class="form-control" name="telefone">
+
+                            <label for="data" class="form-label">Data de nascimento</label>
+                            <input type="date" class="form-control" name="data">
+
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" name="email">
+
+                            <label for="senha" class="form-label">Senha</label>
+                            <input type="password" class="form-control" name="senha">
+                            <br>
+                            <input type="submit" class="btn btn-success">
+                        </div>   
+                    </form>
+                    <a href="index.php" class="btn btn-info">Voltar a tela de inicio</a>
             </div>
-            
+
         </div>
 
     </div>
     
-    <h1>Hello, world!</h1>
+   
 
 
 
